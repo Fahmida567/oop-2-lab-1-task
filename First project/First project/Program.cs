837ab50f5ac6c3f169567e621da0e27356a25aaf
Fahmida <fahmidaturin34@gmail.com>
2020-10-19 10:33:09 +0600

    lab 1 completed

---
+++ First project/First project/Program.cs
@@ -0,0 +1,53 @@
+﻿//1.
+using System;
+
+namespace First_project
+{
+ class Program
+{
+static void Main(string[] args)
+ {
+  Console.WriteLine("Hello ");
+ Console.WriteLine("Fahmida");
+
+            System.Console.WriteLine(15 + 17);
+             System.Console.WriteLine(15/17);
+            System.Console.WriteLine(-5+ 8 * 6);
+
+            System.Console.WriteLine((55 + 9) % 9);
+
+            System.Console.WriteLine(20+ -3 * 5 / 8);
+
+            System.Console.WriteLine(5 + 15 / 3* 2- 8 % 3);
+            int x = 10;
+            int y = 30;
+            int sum = x + y;
+            int multiply = x * y;
+            int substract = x - y;
+            double division = y / x;
+            Console.WriteLine("The sum of two numbers :" + sum);
+            Console.WriteLine("The multiplication of two numbers :" + multiply);
+            Console.WriteLine("The substract of two numbers :" + substract);
+            Console.WriteLine("The division of two numbers :" + division);
+            int rem = 20 / 7;
+            Console.WriteLine("Remainder :" + rem);
+            Double exp = ((25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5));
+            Console.WriteLine("expression:" + exp);
+            double width =  5.5,
+                Height = 8.5;
+            Console.WriteLine("Area is:" + width*Height);
+            Console.WriteLine("Perimeter is:" +2 * (width + Height));
+
+
+
+
+
+        }
+}
+}
+
+
+ 
+
+
+
